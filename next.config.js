f9060fc5a2e7e9c9349890cb3aeec85b218b8d26
sai kise <134133636+saikise@users.noreply.github.com>
2023-08-31 15:43:05 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    // Allow loading images from your Supabase local dev environment
+    // Allow loading images from your Supabase Storage
     domains: [`${process.env.SUPABASE_DOMAIN}`],
   },
 };
